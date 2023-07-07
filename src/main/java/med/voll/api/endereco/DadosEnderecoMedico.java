f5a22885953a49a3cbcd336f149ff97fbd27a4c9
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-07-07 16:13:00 -0300

    repsitory medico ok, falta criar tabela medico no banco

--- src/main/java/med/voll/api/endereco/DadosEnderecoMedico.java
+++ src/main/java/med/voll/api/endereco/DadosEnderecoMedico.java
@@ -1,4 +1,4 @@
 package med.voll.api.endereco;
 
-public record DadosEnderecoMedico(String logradouro, String bairro, String cidade, String uf, String complemento, String numero) {
+public record DadosEnderecoMedico(String logradouro, String cep, String bairro, String cidade, String uf, String complemento, String numero) {
 }
