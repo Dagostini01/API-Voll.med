05d061c50314b66f70e02453879c6e727f25afb0
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-07-07 12:24:28 -0300

    criacao de classe com conexao BD MYSQL e inicio da Repository

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,4 +1,6 @@
 package med.voll.api.medico;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+import med.voll.api.endereco.DadosEnderecoMedico;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEnderecoMedico enderecoMendico) {
 }
--- src/main/java/med/voll/api/paciente/DadosCadastrarPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastrarPaciente.java
@@ -1,6 +1,4 @@
 package med.voll.api.paciente;
 
-import med.voll.api.medico.DadosEndereco;
-
 public record DadosCadastrarPaciente(String nome, String email, String telefone, String cpf, DadosEnderecoPaciente endereco) {
 }
